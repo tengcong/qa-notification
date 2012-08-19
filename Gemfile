01a7c35b38f5e51319a51bd4e27079d656e99497
congteng <congteng45@gmail.com>
2012-08-19 21:36:33 +0800

    first commit hello world

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.3'
+gem 'jquery-rails'
+
+gem "bson_ext", "= 1.6.4"
+gem "mongoid", "= 2.4.11"
+
+
+group :test do
+  gem "fakeweb"
+  gem 'timecop'
+  gem 'cucumber-rails', :require => false
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+  gem 'email_spec'
+  gem 'vcr'
+  gem "factory_girl_rails", "~> 1.2"
+  gem 'rack-test'
+  gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notification::Application.config.secret_token = '1c27b04a2a7058cf898dceca2f6891914735237505f993757f4613a6f4936b03f6c7c01962ea347546b1774e2ae45e5dc32ac8fc40b35c9e36deb2c4f7bef2e6'
